ca178b9b3e6c9d08f742e50bfd468d920f53ef21
Safa Noor <159770136+snoor02@users.noreply.github.com>
2025-03-15 20:22:32 +0500

    sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -5,42 +5,6 @@ export default function sitemap() {
         lastModified: new Date(),
         changeFrequency: 'daily',
         priority: 1,
-      },
-      {
-        url: 'https://royalxcasinox.com/#introduction',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.9,
-      },
-      {
-        url: 'https://royalxcasinox.com/#features',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://royalxcasinox.com/#screenshots',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.7,
-      },
-      {
-        url: 'https://royalxcasinox.com/#how-to-download',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://royalxcasinox.com/#how-to-deposit',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://royalxcasinox.com/#faq',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.7,
       }
     ]
   }
